98e2dd1cedbb5fdaefebeb3c18ac5c60f322683b
PlatonovSerg <platonov1727@gmail.com>
2024-08-21 14:47:22 +0300

    work with routers

--- fastapi_app/core/config.py
+++ fastapi_app/core/config.py
@@ -7,8 +7,14 @@ class RunConfig(BaseModel):
     port: int = 8000
 
 
+class ApiV1Prefix(BaseModel):
+    prefix: str = "/v1"
+    users: str = "/users"
+
+
 class ApiPrefix(BaseModel):
-    prefix: str = "/api/v1"
+    prefix: str = "/api"
+    v1: ApiV1Prefix = ApiV1Prefix()
 
 
 class DataBaseConfig(BaseModel):
@@ -17,6 +23,14 @@ class DataBaseConfig(BaseModel):
     echo_pool: bool = False
     pool_size: int = 50
     max_overflow: int = 10
+
+    naming_conventions: dict[str, str] = {
+        "ix": "ix_%(column_0_labels)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
 
 class Settings(BaseSettings):
