e191e5896a638c589965ba0bf961bb9fb8f2b75f
PlatonovSerg <platonov1727@gmail.com>
2024-08-21 13:42:17 +0300

    declarative_base create

--- fastapi_app/core/models/base.py
+++ fastapi_app/core/models/base.py
@@ -12,6 +12,6 @@ class Base(DeclarativeBase):
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
-        return f"{camel_case_to_snake_case(cls.__name__)}s "
+        return f"{camel_case_to_snake_case(cls.__name__)}s"
 
     id: Mapped[int] = mapped_column(primary_key=True)
